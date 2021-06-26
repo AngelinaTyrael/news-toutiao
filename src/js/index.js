46faaee8ab2e9f8c6d49ee364af504b8ec22da39
songjiwei <songjiwei@BANKQH-DSF242.bankqh.com>
2021-06-27 02:35:19 +0800

    新闻头条:tada:

---
+++ src/js/index.js
@@ -0,0 +1,75 @@
+import './imports'
+
+import Header from '../components/Header'
+import NavBar from '../components/NavBar'
+import NewsList from '../components/NewsList'
+
+import { NEWS_TYPE } from '../data'
+import services from '../services';
+
+;((doc)  => {
+
+  const oApp = doc.querySelector('#app')
+  let oListWrapper = null
+
+  const config = {
+    type: 'top',
+    count: 10,
+    pageNum: 0
+  }
+
+  const newsData = {}
+
+  const init = async () => {
+    render()
+    await setNewsList()
+    bindEvent()
+  }
+
+  function bindEvent () {
+    NavBar.bindEvent(setType)
+  }
+
+  function render () {
+    const headerTpl = Header.tpl({
+      url: '/',
+      title: '新闻头条',
+      showLeftIcon: false,
+      showRightIcon: true
+    })
+
+    const navBarTpl = NavBar.tpl(NEWS_TYPE)
+    const listWrapperTpl = NewsList.wrapperTpl(82)
+    oApp.innerHTML += (headerTpl + navBarTpl + listWrapperTpl)
+    oListWrapper = oApp.querySelector('.news-list')
+  }
+
+  function renderList (data) {
+    const { pageNum } = config
+    const newsListTpl = NewsList.tpl({
+      data,
+      pageNum
+    })
+    oListWrapper.innerHTML += newsListTpl
+    NewsList.imgShow()
+  }
+
+  async function setNewsList () {
+    const { type, count, pageNum } = config
+
+    if (newsData[type]) {
+      return
+    }
+
+    newsData[type] = await services.getNewsList(type, count)
+    console.log(newsData[type][0])
+    renderList(newsData[type][pageNum])
+  }
+
+  function setType (type) {
+    config.type = type
+  }
+
+  init()
+
+})(document)
---
+++ src/libs/utils.js
@@ -0,0 +1,30 @@
+function tplReplace (template, templateObject) {
+  return template().replace(/\{\{(.*?)\}\}/g, (node, key) => {
+    return templateObject[key.trim()]
+  })
+}
+
+function scrollToTop () {
+  setTimeout(() => {
+    window.scrollTo(0, 0)
+  }, 0)
+}
+
+function setPageData (data, count) {
+  const len = data.length
+
+  let pageData = []
+  let index = 0
+
+  while (index < len) {
+    pageData.push(data.slice(index, index += count))
+  }
+
+  return pageData
+}
+
+export {
+  tplReplace,
+  scrollToTop,
+  setPageData
+}
